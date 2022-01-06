00e38047141926c14e59b74487cd3aebde1ab092
Jason Liszka <jliszka@alumni.cmu.edu>
2022-01-06 18:39:59 +0000

    new modes

--- README.md
+++ README.md
@@ -3,21 +3,39 @@
 A wordle solver. Choses the word with the highest information entropy among the remaning candidate words. See [this twitter thread](https://twitter.com/jliszka/status/1478850816182304769?s=20) for discussion.
 
 ```
-$ ./wordle.py swore tares
+$ ./wordle.py play swore tares
 Remaining: 12972
 Guess 1:  tares
 Remaining: 47
-Guess 2:  poise
-Remaining: 5
-Guess 3:  winch
+Guess 2:  shown
 Remaining: 1
-Guess 4:  swore
+Guess 3:  swore
 ```
 
 The best first word is `tares` but it takes a long time to compute that, so you can spot it that guess on the command line.
 
 General usage is
 ```
-wordle.py <hidden word> [guess1 [guess2 ...]]
+wordle.py play <hidden word> [guess1 [guess2 ...]]
+```
+
+For hard mode
+```
+wordle.py hard <hidden word> [guess1 [guess2 ...]]
+```
+
+To have the computer pick a word adversarially:
+```
+wordle.py adversary [guess1 [guess2 ...]]
+```
+
+To search the entire tree (easy mode) for words below depth 6:
+```
+wordle.py search
+```
+
+To display all words sorted by information entropy as an initial guess:
+```
+wordle.py top
 ```
 
--- wordle.py
+++ wordle.py
@@ -4,6 +4,22 @@ from collections import defaultdict
 import math
 import sys
 
+easy_mode = True
+
+words = []
+with open('words') as f:
+	words = f.readlines()
+	words = [ w.rstrip() for w in words ]
+
+total = 0
+freqs = defaultdict(int)
+with open('freqs') as f:
+	lines = f.readlines()
+	for line in lines:
+		parts = line.rstrip().split(' ')
+		freqs[parts[1]] += int(parts[0])
+		total += int(parts[0])
+
 def score(guess, hidden):
 	ret = 0
 	used = [False, False, False, False, False]
@@ -21,26 +37,37 @@ def score(guess, hidden):
 					break
 	return ret	
 
-words = []
-with open('words') as f:
-	words = f.readlines()
-	words = [ w.rstrip() for w in words ]
-
-
 def choose(candidates):
 	if len(candidates) == 1:
 		return candidates[0]
 	guesses = []
-	for w in words: # Easy mode. For hard mode, use `for w in candidates`.
+	for w in (words if easy_mode else candidates):
+		scores = defaultdict(int)
+		for h in candidates:
+			scores[score(w, h)] += freqs.get(h, 1)
+
+		ps = [ scores[h] / total for h in scores ]
+		entropy = -sum([ p * math.log(p) / math.log(2) for p in ps ])
+		# Sort by entropy.
+		# All else equal choose a candidate word instead of one from the corpus.
+		# All else equal choose a more common word.
+		guesses.append((entropy, w in candidates, freqs.get(w, 1), w))
+	return max(guesses)[3]
+
+def top(candidates):
+	guesses = []
+	for w in words:
 		scores = defaultdict(int)
 		for h in candidates:
 			scores[score(w, h)] += 1
 
 		ps = [ scores[h]/len(words) for h in scores ]
 		entropy = -sum([ p * math.log(p) / math.log(2) for p in ps ])
+		m = max([ scores[h] for h in scores ])
 
-		guesses.append((entropy, w))
-	return max(guesses)[1]
+		guesses.append((entropy, len(scores), m, w))
+	for (e, b, m, w) in sorted(guesses, reverse=True):
+		print("{:.4f}".format(e), b, m, w)
 
 def play(hidden, guesses):
 	candidates = words
@@ -53,5 +80,53 @@ def play(hidden, guesses):
 			break
 		candidates = [ w for w in candidates if score(guess, w) == pattern ]
 
-play(sys.argv[1], sys.argv[2:])
+
+def worst(candidates, guess):
+	scores = defaultdict(int)
+	for h in candidates:
+		scores[score(guess, h)] += -math.log(freqs.get(h, 1)/total)
+
+	return max([(scores[p], p) for p in scores])[1]
+
+def adversary(guesses):
+	candidates = words
+	for i in range(6):
+		print("Remaining: {}".format(len(candidates)))
+		guess = choose(candidates) if i >= len(guesses) else guesses[i]
+		print("Guess {}: ".format(i+1), guess)
+		pattern = worst(candidates, guess)
+		if pattern == 0x2aa:
+			break
+		candidates = [ w for w in candidates if score(guess, w) == pattern ]
+
+def search(candidates, depth=1):
+	if len(candidates) == 1:
+		return
+	if depth == 6:
+		print(candidates)
+		return
+	guess = choose(candidates) if depth > 1 else 'tares' # shortcut
+	scores = defaultdict(list)
+	for h in candidates:
+		scores[score(guess, h)].append(h)
+	i = 0
+	for s in scores:
+		search(scores[s], depth+1)
+		if depth == 1:
+			i += 1
+			print("{}/{}".format(i, len(scores)))
+
+
+if sys.argv[1] == "play":
+	play(sys.argv[2], sys.argv[3:])
+elif sys.argv[1] == "hard":
+	easy_mode = False
+	play(sys.argv[2], sys.argv[3:])
+elif sys.argv[1] == "adversary":
+	adversary(sys.argv[2:])
+elif sys.argv[1] == "search":
+	search(words)
+elif sys.argv[1] == "top":
+	top(words)
+
 
