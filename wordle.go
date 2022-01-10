10900278bd1dc69671d84b637e697c75442b9522
Jason Liszka <jliszka@alumni.cmu.edu>
2022-01-10 16:15:41 +0000

    metrics flag, perf wins

--- wordle.go
+++ wordle.go
@@ -8,6 +8,8 @@ import (
     "strings"
     "strconv"
     "math"
+    "flag"
+    "runtime/pprof"
 )
 
 type Word struct {
@@ -47,18 +49,48 @@ func score(guess string, hidden string) int {
     return ret
 }
 
+func score2(guess string, hidden string) int {
+    var checked [5]bool
+    greens := 0
+    for i := 0; i < 5; i++ {
+        greens *= 3
+        if guess[i] == hidden[i] {
+            greens += 2
+            checked[i] = true
+        }
+    }
+    var index [26]int
+    for i := 0; i < 5; i++ {
+        if !checked[i] {
+            index[int(hidden[i] - 'a')] += 1
+        }
+    }
+    yellows := 0
+    for i := 0; i < 5; i++ {
+        yellows *= 3
+        if !checked[i] {
+            g := int(guess[i] - 'a')
+            if index[g] > 0 {
+                yellows += 1
+                index[g] -= 1
+            }
+        }
+    }
+    return greens + yellows
+}
+
 type Guess struct {
     word string
-    entropy float64
+    metric float64
     candidate bool
     freq float64
 }
 
 func (g Guess) betterThan(g2 Guess) bool {
-    if g.entropy > g2.entropy {
+    if g.metric > g2.metric {
         return true
     }
-    if g.entropy < g2.entropy {
+    if g.metric < g2.metric {
         return false
     }
     if g.candidate && !g2.candidate {
@@ -68,6 +100,52 @@ func (g Guess) betterThan(g2 Guess) bool {
         return false
     }
     return g.freq > g2.freq
+}
+
+type Metric func ([243]float64, [243]float64) float64
+var metric Metric
+
+func entropy(freq [243]float64, size [243]float64) float64 {
+    ret := 0.0
+    for i := 0; i < 243; i++ {
+        fq := freq[i]
+        if fq > 0 {
+            pr := fq / total
+            ret -= pr * math.Log2(pr)
+        }
+    }
+    return ret
+}
+
+func expectedBucket(freq [243]float64, size [243]float64) float64 {
+    ret := 0.0
+    for i, fq := range freq {
+        if fq > 0 {
+            pr := fq / total
+            ret -= pr * size[i]
+        }
+    }
+    return ret
+}
+
+func maxBucket(freq [243]float64, size [243]float64) float64 {
+    ret := 0.0
+    for _, sz := range size {
+        if sz > ret {
+            ret = sz
+        }
+    }
+    return -ret
+}
+
+func numBuckets(freq [243]float64, size [243]float64) float64 {
+    ret := 0.0
+    for _, sz := range size {
+        if sz > 0 {
+            ret += 1
+        }
+    }
+    return ret
 }
 
 func choose(candidates []Word, hard bool) string {
@@ -82,25 +160,22 @@ func choose(candidates []Word, hard bool) string {
     guesses := make(chan Guess)
     for i, w := range choices {
         go func(i int, w Word) {
-            scores := map[int]float64{}
+            var freqs [243]float64
+            var sizes [243]float64
             is_candidate := false
             for _, h := range candidates {
-                s := score(w.word, h.word)
-                scores[s] += h.freq
+                s := score2(w.word, h.word)
+                freqs[s] += h.freq
+                sizes[s] += 1
                 if !hard && w.n == h.n {
                     is_candidate = true
                 }
             }
-            entropy := 0.0
-            for _, fq := range scores {
-                pr := fq / total
-                entropy -= pr * math.Log2(pr)
-            }
-            guesses <- Guess{w.word, entropy, is_candidate, w.freq}
+            guesses <- Guess{w.word, metric(freqs, sizes), is_candidate, w.freq}
         }(i, w)
     }
 
-    best_guess := Guess{"", 0.0, false, 0}
+    best_guess := Guess{"xxxxx", -1000000.0, false, 0}
     for range choices {
         guess := <-guesses
         if guess.betterThan(best_guess) {
@@ -151,12 +226,12 @@ func play(hard bool, hidden string) {
     }
 }
 
-func expected(candidates []Word, depth int) float64 {
+func expected(hard bool, candidates []Word, depth int) float64 {
     if len(candidates) == 1 {
         return float64(depth) * candidates[0].freq / total
     }
 
-    guess := choose(candidates, false)
+    guess := choose(candidates, hard)
     scores := map[int][]Word{}
     for _, h := range candidates {
         s := score(guess, h.word)
@@ -165,7 +240,7 @@ func expected(candidates []Word, depth int) float64 {
     i := 0
     e := 0.0
     for _, cs := range scores {
-        e += expected(cs, depth+1)
+        e += expected(hard, cs, depth+1)
         if depth == 1 {
             i += 1
             fmt.Printf("\r%d/%d",  i, len(scores))
@@ -174,11 +249,11 @@ func expected(candidates []Word, depth int) float64 {
     return e
 }
 
-func failure(candidates []Word, depth int) float64 {
+func failure(hard bool, candidates []Word, depth int) float64 {
     if len(candidates) == 1 {
         return 0.0
     }
-    if depth == 6 {
+    if depth == 3 {
         ret := 0.0
         for _, c := range candidates {
             ret += c.freq / total
@@ -186,7 +261,7 @@ func failure(candidates []Word, depth int) float64 {
         return ret
     }
 
-    guess := choose(candidates, false)
+    guess := choose(candidates, hard)
     scores := map[int][]Word{}
     for _, h := range candidates {
         s := score(guess, h.word)
@@ -195,7 +270,7 @@ func failure(candidates []Word, depth int) float64 {
     i := 0
     e := 0.0
     for _, cs := range scores {
-        e += failure(cs, depth+1)
+        e += failure(hard, cs, depth+1)
         if depth == 1 {
             i += 1
             fmt.Printf("\r%d/%d",  i, len(scores))
@@ -222,7 +297,22 @@ func test() {
     eq("colds", "llama", 0x10000)    
 }
 
+var cpuProfile = flag.String("p", "", "write cpu profile to file")
+var hardMode = flag.Bool("h", false, "hard mode")
+var metricStr = flag.String("m", "entropy", "evaluation metric")
+
 func main() {
+    flag.Parse()
+
+    if *cpuProfile != "" {
+        f, err := os.Create(*cpuProfile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+    }
+
     file, err := os.Open("words")
     if err != nil {
         log.Fatalf("readLines: %s", err)
@@ -237,8 +327,9 @@ func main() {
         parts := strings.Fields(line)
         freq, err := strconv.Atoi(parts[1])
         if err == nil {
-            words[i] = Word{parts[0], float64(freq), i}
-            total += float64(freq)
+            f := float64(freq)
+            words[i] = Word{parts[0], f, i}
+            total += f
             i++
         }
     }
@@ -246,14 +337,23 @@ func main() {
 
     test()
 
-    switch os.Args[1] {
-    case "play":
-        play(false, os.Args[2])
-    case "hard":
-        play(true, os.Args[2])
+    switch *metricStr {
     case "exp":
-        fmt.Printf("\n%f\n", expected(words, 1))
+        metric = expectedBucket
+    case "max":
+        metric = maxBucket
+    case "num":
+        metric = numBuckets
+    default:
+        metric = entropy
+    }
+
+    switch flag.Arg(0) {
+    case "play":
+        play(*hardMode, flag.Arg(1))
+    case "exp":
+        fmt.Printf("\n%f\n", expected(*hardMode, words, 1))
     case "fail":
-        fmt.Printf("\n%f\n", failure(words, 1))
+        fmt.Printf("\n%f\n", failure(*hardMode, words, 1))
     }    
 }
